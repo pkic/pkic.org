6337906544309c877a39f113cf5c4fa117ea393f
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2023-08-23 21:22:17 +0200

    Automatically switch tab and set scroll-margin-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 import * as bootstrap from 'js/bootstrap';
+
+var e = document.querySelectorAll('.nav-tabs .nav-link');
+for (var i = 0; i < e.length; i++) {
+    e[i].addEventListener("click", event => {
+        location.hash = event.target.dataset.bsTarget;
+    
+        var se =  document.getElementById(event.target.parentElement.dataset.scrollTarget);
+        se.scrollIntoView();
+    })
+}
+
+if (window.location.hash.indexOf('nav') == 1) {
+    document.getElementById(window.location.hash.substr(1) + '-tab').click();
+}
