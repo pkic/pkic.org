8a6203f3da2cf71303af461dfb942ef13ecf6198
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2023-11-28 15:47:20 +0100

    Show sub results

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     new PagefindUI({ 
         element: "#search",  
         baseUrl: "/",
+        showSubResults: true,
         showEmptyFilters: false
     });
 });
