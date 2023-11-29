d03a51fbe4f9f9c522adff7f7198d659d26fbaba
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2023-11-29 09:00:49 +0100

    Enable autofocus

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     new PagefindUI({ 
         element: "#search",  
         baseUrl: "/",
+        autofocus: true,
         showSubResults: true,
         showEmptyFilters: false
     });
