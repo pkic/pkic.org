4360bba95e38830bccdcf87e4ccbce506e6e2305
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2024-12-19 13:41:49 +0100

    Show local timezone

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,14 @@ for (var i = 0; i < e.length; i++) {
 if (window.location.hash.indexOf('nav') == 1) {
     document.getElementById(window.location.hash.substr(1) + '-tab').click();
 }
+
+document.querySelectorAll('time[datetime]').forEach($e => {
+    const date = new Date($e.dateTime);
+    $e.title = date.toString();
+    const originalText = $e.textContent;
+
+    if ($e.classList.contains('localTime')) {
+        const options = { hour: 'numeric', minute: '2-digit', timeZoneName: 'short', hour12: false };
+        $e.textContent = date.toLocaleTimeString([], options).replace(',', '');
+    }
+});
