01dd6c704450a1606a547d889dea3a3395d2f639
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2026-04-30 09:40:36 +0200

    fix: register GET /versions route in email-templates router
    GET /versions was missing from router.ts, causing 404 when the template
    editor tried to load version history

--- functions/api/v1/admin/email-templates/[key]/router.ts
+++ functions/api/v1/admin/email-templates/[key]/router.ts
@@ -1,13 +1,17 @@
 import { Hono } from "hono";
 import { fromHono } from "chanfana";
 import { AdminEmailTemplatesKeyActivatePost } from "./activate";
-import { onRequestPost as AdminEmailTemplatesKeyVersionsPost_l } from "./versions";
+import {
+  onRequestGet as AdminEmailTemplatesKeyVersionsGet_l,
+  onRequestPost as AdminEmailTemplatesKeyVersionsPost_l,
+} from "./versions";
 import { onRequestGet as AdminEmailTemplatesKeyExistsGet_l } from "./exists";
 
 const app = new Hono();
 export const openapi = fromHono(app);
 
 openapi.post("/activate", AdminEmailTemplatesKeyActivatePost);
+app.get("/versions", AdminEmailTemplatesKeyVersionsGet_l);
 app.post("/versions", AdminEmailTemplatesKeyVersionsPost_l);
 app.get("/exists", AdminEmailTemplatesKeyExistsGet_l);
 
